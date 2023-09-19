751a494eab95304935f9f8aca8c7e5900cbb620c
hyeg0121 <w2218@e-mirim.hs.kr>
2023-09-19 10:11:43 +0900

    0912 수업

--- 20230620_bookreviewcalendar/js/calendar.js
+++ 20230620_bookreviewcalendar/js/calendar.js
@@ -2,36 +2,69 @@ var now = new Date();
 var year = now.getFullYear();
 var month = now.getMonth() + 1;
 
+// html 요소 가져오기
+const monthDiv = document.getElementsByClassName('month')[0];
+const datesContainerDiv = document.getElementsByClassName('dates')[0];
+
 // < : 이전달
+const leftDiv = document.getElementsByClassName('left')[0];
+leftDiv.onclick = () => {
+    month--;
+    let prevMonth = new Date(year, month - 1);
+    year = prevMonth.getFullYear();
+    month = prevMonth.getMonth() + 1;
+    datesContainerDiv.innerHTML = '';
+    setCalendar(year, month);
+};
 
 // > : 이후달
+const rightDiv = document.getElementsByClassName('right')[0];
+rightDiv.onclick = () => {
+    month++;
+    if (month == 13) {
+        year++;
+        month = 1;
+    }
+    datesContainerDiv.innerHTML = '';
+    setCalendar(year, month);
+};
+
 
 const setCalendar = (year, month) => {
-    
-    console.log(year, month);
-    
     // 현재 월 제목에 표시
-    const monthDiv = document.getElementsByClassName('month')[0];
     monthDiv.innerHTML = `${month}월`;
     
-
     // 해당 월의 마지막 날짜
     var thisMonthLastDate = new Date(year, month, 0).getDate();
-    
-    // 해당 월의 첫 날 요일
-    var thisMonthFirstDay = new Date(year, month - 1, 1).getDay();
-    console.log('lastdate :', thisMonthLastDate, 'firstday :', thisMonthFirstDay);
-
-    const datedContainerDiv = document.getElementsByClassName('dates')[0];
 
     for (let date = 1; date <= thisMonthLastDate; date++) {
         let dateItemDiv = document.createElement('div');
         dateItemDiv.classList.add('date');
         dateItemDiv.classList.add('item');
         dateItemDiv.innerHTML = date;
-        datedContainerDiv.append(dateItemDiv);
+        datesContainerDiv.appendChild(dateItemDiv);
     }
+
+    // 1일을 해당 요일로 이동하자 
+    let firstDateDiv = datesContainerDiv.querySelector('.date.item:nth-child(1)');
+
+    // 1일의 요일 구하자
+    var firstDateDay = new Date(year, month - 1, 1).getDay();   
+    firstDateDiv.style.gridColumnStart = firstDateDay + 1;
     
+    // 토요일: 파란색 글자로
+    let saturdayFirstDate = Math.abs(7 - firstDateDay);
+    let saturdayDivs = datesContainerDiv.querySelectorAll(`.date.item:nth-child(7n + ${saturdayFirstDate})`);
+    for (let saturdayDiv of saturdayDivs) {
+        saturdayDiv.style.color = 'blue';
+    }
+
+    // 일요일: 빨간색 글자로
+    let sundayFirstDate = Math.abs(7 - firstDateDay);
+    let sundayDivs = datesContainerDiv.querySelectorAll(`.date.item:nth-child(7n + ${sundayFirstDate})`);
+    for (let sundayDiv of sundayDivs) {
+        sundayDiv.style.color = 'red';
+    }
 };
 
 setCalendar(year, month);
